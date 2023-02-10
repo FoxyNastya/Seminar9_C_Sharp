2f4b23c3732580813428b97026c77ea96d928b08
Anastasiya <atassmorodina@gmail.com>
2023-02-10 21:59:13 +0300

    Классная (нет) работа

--- Classwork/Program.cs
+++ Classwork/Program.cs
@@ -57,3 +57,34 @@ Console.WriteLine (NumberRec(4,8) +"8");
 453 -> 12
 45 -> 9*/
 
+int SumOfNumbers (int number)
+{
+    if(number == 0 )
+        return number;
+    else 
+        return number % 10 + SumOfNumbers(number / 10);
+    
+}
+
+int sum = SumOfNumbers(453);
+Console.WriteLine(sum);
+
+/*Задача 69: Напишите программу, которая на вход 
+принимает два числа A и B, и возводит число А в целую 
+степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8
+
+int Power (int a, int b)
+{
+    if(b == 0)
+        return 1;
+    else
+        return a * Power(a, b-1);
+}
+
+int result = Power(5, 3);
+Console.WriteLine(result);
+
+*/
+
