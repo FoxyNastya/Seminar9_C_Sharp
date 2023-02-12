dcb196860662e2756c7343efbca169f31595f0f4
Anastasiya <atassmorodina@gmail.com>
2023-02-12 16:29:21 +0300

    HW01-02

---
+++ HW02/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая 
+найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+int GetNaturalNumber(string message)
+{
+  int result = 0;
+
+  while (true)
+  {
+    Console.Write(message);
+
+    if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+    {
+      break;
+    }
+    else
+    {
+      Console.WriteLine("Ввели не число или некорректное число. Повторите ввод!");
+    }
+  }
+
+  return result;
+}
+
+int SumMandN(int M, int N)
+{
+   if (M == N) return M;                
+    else if (M < N) return N + SumMandN(M, N - 1);
+    else return N + SumMandN(M, N + 1); 
+}
+
+
+Console.WriteLine();
+int numberN = GetNaturalNumber("Введите натуральное число: ");
+int numberM = GetNaturalNumber("Введите натуральное число: ");
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {numberN} до {numberM} = {SumMandN(numberN, numberM)}");
